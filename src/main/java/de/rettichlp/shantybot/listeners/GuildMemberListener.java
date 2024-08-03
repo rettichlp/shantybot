a429846fdf4227b0dd2a68c19babcdcc171ea607
RettichLP <rettichlp01@gmail.com>
2024-08-03 22:53:24 +0200

    Add join and remove embed messages

--- src/main/java/de/rettichlp/shantybot/listeners/GuildMemberListener.java
+++ src/main/java/de/rettichlp/shantybot/listeners/GuildMemberListener.java
@@ -1,35 +1,65 @@
 package de.rettichlp.shantybot.listeners;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import static de.rettichlp.shantybot.ShantyBot.discordBot;
 import static de.rettichlp.shantybot.ShantyBot.discordBotProperties;
+import static java.awt.Color.GREEN;
+import static java.awt.Color.YELLOW;
 import static java.util.Objects.nonNull;
 
 public class GuildMemberListener extends ListenerAdapter {
 
     @Override
-    public void onGuildMemberJoin(GuildMemberJoinEvent e) {
+    public void onGuildMemberRemove(GuildMemberRemoveEvent e) {
         Guild guild = e.getGuild();
+        Member member = e.getMember();
 
         TextChannel communityTextChannel = discordBotProperties.getCommunityTextChannel();
-        if (nonNull(communityTextChannel)) {
+        if (nonNull(member) && nonNull(communityTextChannel)) {
+            // remove message embed
+            MessageEmbed messageEmbed = new EmbedBuilder()
+                    .setColor(YELLOW)
+                    .setTitle("Auf Wiedersehen!")
+                    .setAuthor("ShantyBot", null, discordBot.getSelfUser().getEffectiveAvatarUrl())
+                    .setDescription(member.getEffectiveName() + " hat **ShantyTown** verlassen!")
+                    .setFooter("Beigetreten am", member.getEffectiveAvatarUrl())
+                    .setTimestamp(member.getTimeJoined())
+                    .build();
+
+            communityTextChannel.sendMessageEmbeds(messageEmbed).queue();
             communityTextChannel.getManager().setTopic("Willkommen! Du bist zwar nur einer von " + guild.getMemberCount() + ", aber dieser Text wurde nur für dich angepasst. :D").queue();
         }
-
-        guild.addRoleToMember(e.getMember(), discordBotProperties.getMemberRole()).queue();
     }
 
     @Override
-    public void onGuildMemberRemove(GuildMemberRemoveEvent e) {
+    public void onGuildMemberJoin(GuildMemberJoinEvent e) {
         Guild guild = e.getGuild();
+        Member member = e.getMember();
 
         TextChannel communityTextChannel = discordBotProperties.getCommunityTextChannel();
         if (nonNull(communityTextChannel)) {
+            // join message embed
+            MessageEmbed messageEmbed = new EmbedBuilder()
+                    .setColor(GREEN)
+                    .setTitle("Willkommen!")
+                    .setAuthor("ShantyBot", null, discordBot.getSelfUser().getEffectiveAvatarUrl())
+                    .setDescription(member.getEffectiveName() + " ist **ShantyTown** beigetreten!")
+                    .setFooter("Account erstellt am", member.getEffectiveAvatarUrl())
+                    .setTimestamp(member.getTimeCreated())
+                    .build();
+
+            communityTextChannel.sendMessageEmbeds(messageEmbed).queue();
             communityTextChannel.getManager().setTopic("Willkommen! Du bist zwar nur einer von " + guild.getMemberCount() + ", aber dieser Text wurde nur für dich angepasst. :D").queue();
         }
+
+        guild.addRoleToMember(member, discordBotProperties.getMemberRole()).queue();
     }
 }
